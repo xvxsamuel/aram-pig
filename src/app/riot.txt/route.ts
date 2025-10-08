8bb15c08a3e4b10b773c001268da001592388612
xvxsamuel <31626959+xvxsamuel@users.noreply.github.com>
2025-10-08 18:53:30 +0200

    Riot file

--- src/app/riot.txt/route.ts
+++
@@ -1,8 +0,0 @@
-// riot verification file handler
-export async function GET() {
-  return new Response('5e668db4-c18b-4401-9d15-941698166eaa', {
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  })
-}
